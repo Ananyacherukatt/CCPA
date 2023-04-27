02cdca589bf136d43bd19a175c56720ca4559a45
Ananyacherukatt <ananyacherukattu@gmail.com>
2023-04-27 14:14:48 +0530

    last commit

---
+++ ccpa.js
@@ -0,0 +1,4 @@
+document.getElementById("checkbox-warning").addEventListener("click", () => {
+  document.getElementById("pop-up-ccpa").classList.toggle("active");
+  document.getElementById("banner-ccpa").style.display = "none";
+});
